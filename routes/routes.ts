8b73794ebe665e8eb20820f69e84ca04c87c6f03
Michal Radomski <michalra@op.pl>
2022-06-21 19:10:21 +0200

    UserSchema was added

--- routes/routes.ts
+++ routes/routes.ts
@@ -1,9 +1,12 @@
 import express from "express";
 
+const userModel = require("../models/UserModel");
+
 const router = express.Router();
 
 router.get("/", (req, res) => {
-  res.send("hello world");
+  console.log("req.ip:", req.ip);
+  res.send("<h1>Hello World!</h1>");
 });
 
 module.exports = router;
